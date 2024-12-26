b1f82d07e90f9762d737763d87d6c677bb74b134
Nameless236 <pastojakub@gmail.com>
2024-12-26 21:15:40 +0100

    Login and authorization

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,47 @@
+<!-- resources/views/layouts/navigation.blade.php -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <div class="logo-container">
+            <img src="{{ asset('images/logo.png') }}" alt="TechNova Logo" class="navbar-logo">
+        </div>
+        <a class="navbar-brand" href="{{ route('home') }}">TechNova Labs</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About Us</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('programs.index') }}">Programs</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('innovations') }}">Innovations</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('contact.index') }}">Contact</a></li>
+                @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Profile</a></li>
+                            <li>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button class="dropdown-item" type="submit">Log Out</button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-person-circle"></i>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
+                            <li><a class="dropdown-item" href="{{ route('register') }}">Register</a></li>
+                        </ul>
+                    </li>
+                @endauth
+            </ul>
+        </div>
+    </div>
+</nav>
